682a1d69e8431f1c8f28bf4b0f48317ea93af5a4
Инсаф Гарифуллин <vaironfop@Mail.ru>
2025-11-24 19:14:29 +0300

    Добавлена реализация даты

--- src/main/java/Message.java
+++ src/main/java/Message.java
@@ -1,15 +1,17 @@
+import java.text.SimpleDateFormat;
 import java.util.Date;
 
 public class Message {
     private User from;
     private User to;
     private String text;
-    private Date date;
+    private String date;
 
-    public Message(User from, User to, String text, Date date) {
+    public Message(User from, User to, String text) {
         this.from = from;
         this.to = to;
         this.text = text;
+        this.date = new SimpleDateFormat("dd.MM.yyyy").format(new Date());
     }
 
     @Override
